b9f728820d9ec3ec00dfe4e6e858f811173ca37a
sychev2003di <sychev2003di@gmail.com>
2024-09-23 00:04:09 +0300

    start lab1

---
+++ lab1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Raylib_cs;
+using rl = Raylib_cs.Raylib;
+
+class Program {
+    public static void Main(String[] args) {
+        rl.InitWindow(800, 480, "Hello World");
+
+        while (!rl.WindowShouldClose()) {
+            rl.BeginDrawing();
+            rl.ClearBackground(Color.White);
+
+            rl.DrawText(
+                "Hello, world!",
+                12, 12,
+                20, Color.Black
+            );
+
+            rl.EndDrawing();
+        }
+
+        rl.CloseWindow();
+    }
+}
